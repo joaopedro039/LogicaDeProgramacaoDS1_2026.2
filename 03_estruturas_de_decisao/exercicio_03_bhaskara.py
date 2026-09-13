69fb8ad988224888d403e84364799a44f1a92abc
joaopedro039 <joaopedresn402@gmail.com>
2026-09-13 17:36:37 -0300

    concluído

--- 03_estruturas_de_decisao/exercicio_03_bhaskara.py
+++ 03_estruturas_de_decisao/exercicio_03_bhaskara.py
@@ -9,3 +9,15 @@ Leia 3 valores de ponto flutuante (A, B e C) de uma equação do 2º grau.
 """
 
 # TODO: Desenvolva o algoritmo abaixo:
+import math
+a = float(input("Digite o ponto A: "))
+b = float(input("Digite o ponto B: "))
+c = float(input("Digite o ponto C: "))
+delta = b**2-4*a*c
+if a == 0 or delta < 0:
+    print("Impossivel calcular")
+else:
+    r1 = (-b+(math.sqrt(delta))) / (2*a)
+    r2 = (-b-(math.sqrt(delta))) / (2*a)
+    print(f"raiz 1 é igual á: {r1:.5f}")
+    print(f"raiz 2 é igual á: {r2:.5f}")
