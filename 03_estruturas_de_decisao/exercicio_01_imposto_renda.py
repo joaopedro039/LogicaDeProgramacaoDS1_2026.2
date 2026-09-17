187ed0807f75300a69a46763d1bf2cb28a63bafe
joaopedro039 <joaopedresn402@gmail.com>
2026-09-17 15:47:48 -0300

    concluido

--- 03_estruturas_de_decisao/exercicio_01_imposto_renda.py
+++ 03_estruturas_de_decisao/exercicio_01_imposto_renda.py
@@ -17,11 +17,14 @@ salario_pessoa = float(input("Digite o seu sálario: "))
 if salario_pessoa <= 2000:
     print("O seu salário não tem nenhuma taxa.")
 elif salario_pessoa >= 2000.01 and salario_pessoa <= 3000:
-    imposto_final_ate_3000 =(salario_pessoa * 0.08)
+    salario_pessoa1 = salario_pessoa - 2000
+    imposto_final_ate_3000 =(salario_pessoa1 * 0.08)
     print(f"A taxa sobre o seu salário é de (8%), imposto final: {imposto_final_ate_3000:.2f}")
 elif salario_pessoa >= 3000.01 and salario_pessoa <= 4500:
-    imposto_final_ate_4500 =(salario_pessoa * 0.08 + salario_pessoa * 0.18)
+    salario_pessoa2 = salario_pessoa - 3000
+    imposto_final_ate_4500 =(80 + salario_pessoa2 * 0.18)
     print(f"A taxa sobre o seu salário é de (18% + 8%), imposto final: {imposto_final_ate_4500:.2f}")
 else:
-    imposto_final_acima_de_4500 = (salario_pessoa * 0.08 + salario_pessoa * 0.18 + salario_pessoa * 0.28)
+    salario_pessoa3 = salario_pessoa - 4500
+    imposto_final_acima_de_4500 = (80 + 270 + salario_pessoa3 * 0.28)
     print(f"A taxa sobre o seu salário é de (28% + 18% + 8%), imposto final: {imposto_final_acima_de_4500:.2f}")
